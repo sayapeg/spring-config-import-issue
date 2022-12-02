beaf5a684a48cbee98671c1fff57364e68205317
sayapeg <85237747+sayapeg@users.noreply.github.com>
2022-12-02 19:58:04 +0530

    Project with versions causing the issue

---
+++ src/main/java/com/example/SpringCloudConfigImportsIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigImportsIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigImportsIssueApplication.class, args);
+	}
+
+}
